b69f018f4de22b05a99b4dd9744ed748a57ecbcd
ahmad-musa <ahmadmusa0987@gmail.com>
2022-01-11 01:44:40 +0600

    JS added + Scrolling fixed Nav

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){       
+    var scroll_start = 0;
+    var startchange = $('#home');
+    var offset = startchange.offset();
+
+     if (startchange.length){
+        $(document).scroll(function() { 
+        scroll_start = $(this).scrollTop();
+
+        if(scroll_start > offset.top) {
+            $(".fixed-top").css({
+                "background-color": "#beb0fc",
+                "transition": "all linear .5s",
+                "width":"100%",
+                "padding": "0.5rem 1rem",
+                });
+        } else {
+            $(".fixed-top").css({
+                "background-color": "transparent",
+                "padding": "1.5rem 1rem",
+                });
+            }
+        });
+     }
+
+ });
