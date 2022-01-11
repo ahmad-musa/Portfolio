dbd20f6c4cc37607b16b142e1ad8f9b70692c2d9
ahmad-musa <ahmadmusa0987@gmail.com>
2022-01-11 16:58:11 +0600

    Owl Carousel all working

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-$(document).ready(function(){       
+$(document).ready(function () {
     var scroll_start = 0;
     var startchange = $('#home');
     var offset = startchange.offset();
 
-     if (startchange.length){
-        $(document).scroll(function() { 
-        scroll_start = $(this).scrollTop();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
 
-        if(scroll_start > offset.top) {
-            $(".fixed-top").css({
-                "background-color": "#beb0fc",
-                "transition": "all linear .5s",
-                "width":"100%",
-                "padding": "0.25rem 1rem",
+            if (scroll_start > offset.top) {
+                $(".fixed-top").css({
+                    "background-color": "#beb0fc",
+                    "transition": "all linear .5s",
+                    "width": "100%",
+                    "padding": "0.25rem 1rem",
                 });
-        } else {
-            $(".fixed-top").css({
-                "background-color": "transparent",
-                "padding": "1.5rem 1rem",
+            } else {
+                $(".fixed-top").css({
+                    "background-color": "transparent",
+                    "padding": "1.5rem 1rem",
                 });
             }
         });
-     }
+    }
 
- });
+});
 
 
 // ADDING SIMPLE STYLE NAVBAR SCROLLING STYLE
@@ -34,3 +34,38 @@ $(document).ready(function(){
 //       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 //     });
 //   });
+
+
+
+
+$(document).ready(function () {
+
+    var owl = $('.owl-carousel');
+
+    owl.owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 5,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: true
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: true
+            }
+        }
+    })
+
+});
